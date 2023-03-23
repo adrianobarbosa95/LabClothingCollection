574af6c66ff085e862e2118917b090f48896ef31
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-03-23 12:59:40 -0300

    Adiciona tela redefinir senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
 import { LoginComponent } from './components/login/login.component';
+import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
 
 const routes: Routes = [
-{path:'login', component: LoginComponent }
+{path:'entrar', component: LoginComponent },
+{path:'recuperar-senha', component: ResetPassComponent },
+{path:'cadastrar-usuario', component: CadastroUsuarioComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
+import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ResetPassComponent,
+    CadastroUsuarioComponent
   ],
   imports: [
     BrowserModule,
