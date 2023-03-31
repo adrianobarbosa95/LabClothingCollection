769295916945326e5be3c74b28453fd17261a11f
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-03-31 09:15:22 -0300

    Adiciona a pasta pages e move os arquivos corretos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
-import { CollectionListComponent } from './components/collection-list/collection-list.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
+import { CollectionListComponent } from './pages/collection-list/collection-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { FormCollectionComponent } from './components/form-collection/form-collection.component';
 import { FormModelComponent } from './components/form-model/form-model.component';
-import { LoginComponent } from './components/login/login.component'; 
-import { ModelListComponent } from './components/model-list/model-list.component';
-import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
+import { LoginComponent } from './pages/login/login.component'; 
+import { ModelListComponent } from './pages/model-list/model-list.component';
+import { ResetPassComponent } from './pages/reset-pass/reset-pass.component';
 import { AuthGuard } from './guards/auth.guard';
 import { FullComponent } from './layouts/full/full.component';
 
